dc13eb50405e070121dc7bb3083b1bdebfd65145
Jonathan Roberto <jonathan.995@hotmail.com>
2022-03-05 19:27:36 -0300

    Express setup

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 'use strict'
 
-console.log('Hello world')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const helmet = require('helmet')
+const morgan = require('morgan')
+
+const app = express()
+
+app.use(helmet())
+app.use(bodyParser.json())
+app.use(cors())
+app.use(morgan('combined'))
+
+app.listen(3001, () => {
+  console.log('listening on port 3001')
+})
